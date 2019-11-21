33690b5b011c01257dfbf163ec3338d7d8434ee0
Raphael Tarita <raphael.tarita@gmail.com>
2019-11-21 11:57:34 +0100

    [DEVELOP] replaced sytem.out by slf4j logger

--- src/main/code/java/com/tara/util/main/InfoClass.java
+++ src/main/code/java/com/tara/util/main/InfoClass.java
@@ -1,12 +1,12 @@
 package com.tara.util.main;
 
-import java.io.PrintStream;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class InfoClass {
-    private static final PrintStream OUT = System.out;
 
     public static void main(String[] args) {
-        OUT.println(
+        log.info(
                 "This is the main class for java utils."
                         + "\nThe function main() displays this information."
                         + "\nThere is no other purpose of this Class."
