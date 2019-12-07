653bc0a43b26af5d58d12502a8ac3aa6e3b1f0c9
Raphael Tarita <raphael.tarita@gmail.com>
2019-12-07 21:23:37 +0100

    [FEAT-012JAVA] Merged changes from branch 'develop'

--- Utils Common/src/main/java/com/tara/util/main/InfoClass.java
+++ Utils Common/src/main/java/com/tara/util/main/InfoClass.java
@@ -1,7 +1,5 @@
 package com.tara.util.main;
 
-import com.tara.util.id.StringUID;
-import com.tara.util.id.UID;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -11,15 +9,9 @@ public class InfoClass {
 
     public static void main(String[] args) {
         log.info(
-            "This is the main class for java utils."
-                + "\nThe function main() displays this information."
-                + "\nThere is no other purpose of this Class."
+                "This is the main class for java utils."
+                        + "\nThe function main() displays this information."
+                        + "\nThere is no other purpose of this Class."
         );
-
-        UID id1 = new StringUID();
-        UID id2 = new StringUID("abc");
-        UID id3 = new StringUID("abc");
-
-        log.info(id1.toString() + ", " + id2.toString() + ", " + id3.toString());
     }
 }
